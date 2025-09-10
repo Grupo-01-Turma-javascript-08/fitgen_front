8f8ef1650b426008d48899a25d917b57245fa831
Daliane <daliane.rabelo01@gmail.com>
2025-09-10 15:17:12 -0300

    atualiza navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,18 +2,23 @@ function Navbar() {
   return (
     <nav className="absolute top-0 left-0 w-full p-6 lg:p-10 flex justify-center z-50">
       <div className="flex justify-between items-center w-full max-w-4xl bg-black bg-opacity-10 backdrop-blur-md rounded-full px-5 py-3 border border-white border-opacity-20">
+        
         {/* Logo */}
         <div className="flex items-center text-white text-2xl font-bold">
-          <img src="https://png.pngtree.com/png-clipart/20250605/original/pngtree-fitness-gym-logo-with-dumbbell-lifting-silhouettes-png-image_21126577.png" className="h-12 mr-4" />
+          <img
+            src="https://png.pngtree.com/png-clipart/20250605/original/pngtree-fitness-gym-logo-with-dumbbell-lifting-silhouettes-png-image_21126577.png"
+            className="h-12 mr-4"
+            alt="Logo da academia"
+          />
         </div>
-        
+
         {/* Links de Navegação */}
         <ul className="hidden md:flex space-x-8 text-white font-medium">
           <li><a href="#home" className="hover:text-orange-500 transition-colors">HOME</a></li>
           <li><a href="#about" className="hover:text-orange-500 transition-colors">Sobre nós</a></li>
           <li><a href="#service" className="hover:text-orange-500 transition-colors">Produtos</a></li>
         </ul>
-        
+
         {/* Botão */}
         <button className="bg-orange-500 text-white font-semibold py-2 px-6 rounded-full hover:bg-orange-600 transition-colors">
           Novo Plano
@@ -22,3 +27,5 @@ function Navbar() {
     </nav>
   );
 }
+
+export default Navbar;
