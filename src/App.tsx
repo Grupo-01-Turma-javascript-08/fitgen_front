10e7646600c815407f6a95170461c3be70af6e5e
Daliane <daliane.rabelo01@gmail.com>
2025-09-10 14:05:24 -0300

    Crição Sobre-Nós

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Sobre from "./pages/sobre/sobre";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>
+    <BrowserRouter>
+      <div className="bg-gray-900 text-white min-h-screen font-sans">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
